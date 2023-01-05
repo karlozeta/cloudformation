12d445308a17ce47b7be4b09315914f341f73f2a
karlozeta <66536065+karlozeta@users.noreply.github.com>
2023-01-05 18:39:29 -0500

    Update aws-codepipeline-prereq.sh

--- codepipeline/aws-codepipeline-prereq.sh
+++ codepipeline/aws-codepipeline-prereq.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 aws cloudformation deploy \
---region us-east-2 \
+--region us-east-1 \
 --stack-name codepipeline-platzi-prereq \
 --template-file ./aws-codepipeline-prereq.yml \
 --capabilities CAPABILITY_IAM \
